f523430a8942fc4040c16ab489ea28e527636a7e
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-09-01 22:15:02 +0545

    Update main.py

--- Drought/main.py
+++ Drought/main.py
@@ -7,6 +7,7 @@ import sqlite3
 from typing import Optional
 import os
 
+// rough sketch
 MODEL_DIR = "models"
 DB = "alerts.db"
 ALERT_DROUGHT_PROB_THRESHOLD = 0.6
@@ -103,4 +104,5 @@ def predict(sample: Sample):
         "predicted_demand_m3": demand,
         "predicted_supply_m3": supply
     }
+
 
